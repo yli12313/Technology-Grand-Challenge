4bb22f807c3de9e755304d43f677b8e049486bc3
Yingquan Li <yli12313@umd.edu>
2021-03-08 16:39:18 -0500

    Finished first SQL.

--- Interview-Prep-2021/SQL/new_companies.sql
+++ Interview-Prep-2021/SQL/new_companies.sql
@@ -1,11 +1,19 @@
 -- LINK: https://www.hackerrank.com/challenges/the-company/problem
--- company_code
--- founder name
--- total number of lead managers
--- total number of senior managers
--- total number of managers
--- total number of employees
 
--- order by ascending company_code
+SELECT 
+  c.company_code, 
+  c.founder, 
+  COUNT(DISTINCT lm.lead_manager_code),
+  COUNT(DISTINCT sm.senior_manager_code),
+  COUNT(DISTINCT m.manager_code),
+  COUNT(DISTINCT e.employee_code)
+FROM Company c, Lead_Manager lm, Senior_Manager sm, Manager m, Employee e
+WHERE
+  c.company_code = lm.company_code
+  AND lm.lead_manager_code = sm.lead_manager_code
+  AND sm.senior_manager_code = m.senior_manager_code
+  AND m.manager_code = e.manager_code
+GROUP BY c.company_code, c.founder
+ORDER BY c.company_code ASC;
 
 
