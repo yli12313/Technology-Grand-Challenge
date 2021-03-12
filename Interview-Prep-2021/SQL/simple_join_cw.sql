1a7ed047010afeaf732f6ed7f19d6339daf10536
Yingquan Li <yli12313@umd.edu>
2021-03-12 14:36:20 -0500

    Made changes.

--- Interview-Prep-2021/SQL/simple_join_cw.sql
+++
@@ -1,3 +0,0 @@
-SELECT prod.id, prod.name, prod.isbn, prod.company_id, prod.price, comp.name AS company_name
-FROM products prod
-LEFT JOIN companies comp ON prod.company_id = comp.id;
--- Interview-Prep-2021/SQL/top_earners_hr.sql
+++
@@ -1,9 +0,0 @@
--- LINK: https://www.hackerrank.com/challenges/earnings-of-employees/problem?h_r=internal-search
-
-SELECT 
-  months*salary AS earnings,
-  COUNT(*)
-FROM Employee
-GROUP BY earnings
-ORDER BY earnings DESC
-LIMIT 1;
