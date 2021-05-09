3508acdc03e88661cad8b41085dac0be9caddff5
Yingquan Li <yli12313@umd.edu>
2021-05-09 15:20:06 -0400

    Adding the onces working on.

---
+++ Interview-Prep-2021/Leetcode/longest_happy_string_TBD.py
@@ -0,0 +1,7 @@
+# LINK: https://leetcode.com/problems/longest-happy-string/
+
+class Solution(object):
+  def longestDiverseString(self, a, b, c):
+
+foo = Solution();
+print(foo.longestDiverseString(YYY));
