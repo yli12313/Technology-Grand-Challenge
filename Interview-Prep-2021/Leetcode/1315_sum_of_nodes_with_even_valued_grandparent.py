afe19b092bc95612e23c310ec9fdea8abf72a763
Yingquan Li <orbit196@gmail.com>
2022-06-14 14:59:10 -0400

    Added documentation to 1315.

--- Interview-Prep-2021/Leetcode/1315_sum_of_nodes_with_even_valued_grandparent.py
+++ Interview-Prep-2021/Leetcode/1315_sum_of_nodes_with_even_valued_grandparent.py
@@ -1,3 +1,5 @@
+# Link: https://leetcode.com/problems/sum-of-nodes-with-even-valued-grandparent/
+
 class TreeNode(object):
   def __init__(self, val=0, left=None, right=None):
     self.val = val
@@ -6,25 +8,35 @@ class TreeNode(object):
 
 class Solution(object):
   def sumEvenGrandparent(self, root):
+
+    # Pass the root into an auxiliary function. The genius to this solution is that the sum/answer
+    # is passed into the function itself and tabulated via recursion!
     return self.auxiliary(0, root, None, None)
     
   def auxiliary(self, s, cur, gp, p):
+    # Set a temporary sum as the sum/answer.
     tmp_sum = s
         
+    # If the current node is None, then return the sum/answer.
     if cur == None:
       return tmp_sum
         
+    # If the grandparent is None and it's an even node, increase the temporary sum by the current node.
     if gp != None and gp.val % 2 == 0:
       tmp_sum += cur.val
         
+    # If the left side of the current node is present, recurse on the left node and set the temporary sum as the return value.
     if cur.left:
       tmp_sum = self.auxiliary(tmp_sum, cur.left, p, cur)
+
+    # If the right side of the current node is present, recurse on the right node and set the temporary sum as the return value.
     if cur.right:
       tmp_sum = self.auxiliary(tmp_sum, cur.right, p, cur)
-        
+    
+    # Return the sum/answer.
     return tmp_sum
 
-
+# Creating the tree for testing if the algorithm worked correctly.
 root_node = TreeNode(6)
 root_node.left = TreeNode(7)
 root_node.right = TreeNode(8)
