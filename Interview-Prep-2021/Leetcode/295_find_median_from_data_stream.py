74194ace96c983a4e8de2fb29f9d925bfecd2428
Yingquan Li <orbit196@gmail.com>
2022-06-14 15:11:56 -0400

    Create 295_find_median_from_data_stream.py

---
+++ Interview-Prep-2021/Leetcode/295_find_median_from_data_stream.py
@@ -0,0 +1,13 @@
+class MedianFinder(object):
+  def __init__(self):
+  
+  def addNum(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+      
+  def findMedian(self):
+        """
+        :rtype: float
+        """
