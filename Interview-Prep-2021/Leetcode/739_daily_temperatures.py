2c75d43fcebc6c579486b06cbedd50c71129801b
Yingquan Li <orbit196@gmail.com>
2022-07-06 20:45:59 -0400

    Update 739_daily_temperatures.py

--- Interview-Prep-2021/Leetcode/739_daily_temperatures.py
+++ Interview-Prep-2021/Leetcode/739_daily_temperatures.py
@@ -1,5 +1,3 @@
-#Link: https://leetcode.com/problems/daily-temperatures/
-
 class Solution(object):
     def dailyTemperatures(self, temperatures):
         # Get the length of the temperatures array.
@@ -10,7 +8,7 @@ class Solution(object):
 
         # Define the answer array that is of length N.
         answer = [0]*(N)
-        
+
         # Loop through the temperature array.
         for i in range(N):
             # Get the current temperature if as the loop goes through the 
@@ -23,6 +21,43 @@ class Solution(object):
             # part process. 1) Pop from the top of the stack, 2) calculate the 
             # distance from the current temperature to the previous lower temperature
             # in terms of days, and 3) store the answer. 
+
+            # This is a monotonically decreasing stack where we store the index values!
+
+            ## Example ##:
+            # temperatures: [30, 20, 10, 60]
+            # Same result if temperatures was: [60, 50, 40, 70]
+
+            # stack: []
+            # answer: [0, 0, 0, 0]
+
+            # -------
+
+            # stack: [0]
+
+            # -------
+
+            # stack: [0, 1]
+
+            # -------
+
+            # stack: [0, 1, 2]
+
+            # -------
+
+            ## Encountered a bigger element! ##
+
+            # 60 - 10 (temperatures) --> 3 - 2 = 1
+            # 60 - 20 (temperatures) --> 3 - 1 = 2
+            # 60 - 30 (temperatures) --> 3 - 0 = 3
+
+            # answer = [3, 2, 1, 0]
+            # stack = [3]
+
+            # -------
+
+            # Return the answer! The loop ends after i = 3.
+
             while stack and temperatures[stack[-1]] < current:
                 prevTemp = stack.pop()
                 answer[prevTemp] = i-prevTemp
@@ -35,4 +70,4 @@ class Solution(object):
         return answer;       
 
 foo = Solution();
-print(foo.dailyTemperatures([30,60,90]));
+print(foo.dailyTemperatures([60, 50, 40, 70]));
