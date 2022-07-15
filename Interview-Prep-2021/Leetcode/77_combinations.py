948b6bf70f61bce61cd563d54e79b85615f624b1
Yingquan Li <orbit196@gmail.com>
2022-07-14 21:23:35 -0400

    Update 77_combinations.py

--- Interview-Prep-2021/Leetcode/77_combinations.py
+++ Interview-Prep-2021/Leetcode/77_combinations.py
@@ -5,26 +5,33 @@ class Solution(object):
         # Define the resulting list
         result = []
 
+        # k is the limit of each list within the larger list.
+        limit = k
+        
         # Kick off backtracking and recursion
-        self.backtracking(xrange(1, n+1), k, 0, [], result)
+        self.backtracking(range(1, n+1), 0, k, [], result)
 
         # Return the result
         return result
-        
-        
-    def backtracking(self, nums, k, index, holder, result):
+    
+    def backtracking(self, numbers, index, limit, holder, result):
 
-        # Define the base case: the index is 0 and we are ready to 
+        # Defining the base case: if the limit is 0, we are ready to 
         # append the currently growing list to the resulting list.
-        # Return from this level and back track to an upper level
-        # because we found a part of the answer.
-        if k == 0:
+        # Return from this level and backtrack to an upper level in 
+        # the recursion stack because we found a part of the answer.
+        if limit == 0:
             result.append(holder)
             return
 
-        # Go through all the numbers in 1...n+1 and use recursion to
-        # get all the groups of numbers of size k for each number in 
-        # nums, but we don't iterate with the values! We iterate with
-        # the index of the numbers!
-        for i in range(index, len(nums)):
-            self.backtracking(nums, k-1, i+1, holder+[nums[i]], result)
+        # Go through all the numbers in 1...n+1 (interate by their index, so start 
+        # with 0) and use recursion to get all the groups of numbers of size: limit. 
+        # But we don't iterate with the values! We iterate with the index of the numbers!
+        # Need to track both the numbers and their index (see parameters: numbers, index).
+        # The parameter: numbers doesn't change, but the index increases +1 for every 
+        # recursive call and the limit -1 for every recursive call.
+        for i in range(index, len(numbers)):
+            self.backtracking(numbers, i+1, limit-1, holder+[numbers[i]], result)
+
+foo = Solution();
+print(foo.combine(4, 2));
