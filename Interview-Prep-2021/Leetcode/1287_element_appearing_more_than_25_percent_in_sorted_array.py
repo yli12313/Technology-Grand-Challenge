c26cd8afa5a7705ecd789ab1c1737b1a9f6c29ea
Yingquan Li <orbit196@gmail.com>
2022-08-04 20:43:22 -0400

    Update 1287_element_appearing_more_than_25_percent_in_sorted_array.py

--- Interview-Prep-2021/Leetcode/1287_element_appearing_more_than_25_percent_in_sorted_array.py
+++ Interview-Prep-2021/Leetcode/1287_element_appearing_more_than_25_percent_in_sorted_array.py
@@ -1,16 +1,22 @@
+# Link: https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/
+
 class Solution(object):
   def findSpecialInteger(self, arr):
+    # If the the array is empty, then return None.
     if not arr:
       return None
-
+    
+    # Define a dictionary.
     dictionary = {}
 
+    # Using the dictionary, create counts of all the values found in: arr.
     for val in arr:
       if val in dictionary.keys():
         dictionary[val] += 1
       else:
         dictionary[val] = 1
     
+    # Return the key with the highest count, which is the element that appears more than 25%.
     return max(dictionary, key=dictionary.get)
 
 foo = Solution();
