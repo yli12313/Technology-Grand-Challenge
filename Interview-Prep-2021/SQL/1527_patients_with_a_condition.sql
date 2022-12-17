dcabd29fedcbbb3e3879867197f3c6e8abd034d6
Yingquan Li <orbit196@gmail.com>
2022-12-17 15:32:15 -0500

    Add files via upload

---
+++ Interview-Prep-2021/SQL/1527_patients_with_a_condition.sql
@@ -0,0 +1,8 @@
+-- LINK: https://leetcode.com/problems/patients-with-a-condition/
+
+SELECT 
+    patient_id, 
+    patient_name, 
+    conditions 
+FROM Patients
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE "% DIAB1%";
