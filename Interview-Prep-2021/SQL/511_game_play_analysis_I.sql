c5c89d68fe7a518a821d40ff7e338f02431d78dd
Yingquan Li <orbit196@gmail.com>
2022-12-26 13:24:46 -0500

    Add files via upload

---
+++ Interview-Prep-2021/SQL/511_game_play_analysis_I.sql
@@ -0,0 +1,9 @@
+-- LINK: https://leetcode.com/problems/game-play-analysis-i/
+
+-- NOTE: Good job, keep grinding!
+
+SELECT
+    player_id,
+    MIN(event_date) AS 'first_login'
+FROM Activity
+GROUP BY player_id;
