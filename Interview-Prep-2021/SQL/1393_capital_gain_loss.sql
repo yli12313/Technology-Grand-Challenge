67d8d63b2158ac8a7117afa11b7d3401b8c4a470
Yingquan Li <orbit196@gmail.com>
2022-12-27 18:28:03 -0500

    Update 1393_capital_gain_loss.sql

--- Interview-Prep-2021/SQL/1393_capital_gain_loss.sql
+++ Interview-Prep-2021/SQL/1393_capital_gain_loss.sql
@@ -10,3 +10,10 @@ SELECT
     SUM(IF(operation='Buy', -price, price)) AS 'capital_gain_loss'
 FROM Stocks
 GROUP BY stock_name;
+
+-- SOLUTION WITH CASE STATEMENT
+-- SELECT stock_name,
+-- SUM(CASE WHEN operation = 'Buy' THEN -1*price
+-- WHEN operation = 'sell' THEN price END) as capital_gain_loss
+-- FROM Stocks
+-- GROUP BY stock_name;
