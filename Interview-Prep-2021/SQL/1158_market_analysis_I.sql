72df27e8d1ad78cca12f9f37551671932b072605
Yingquan Li <orbit196@gmail.com>
2022-12-28 12:30:24 -0500

    Update 1158_market_analysis_I.sql

--- Interview-Prep-2021/SQL/1158_market_analysis_I.sql
+++ Interview-Prep-2021/SQL/1158_market_analysis_I.sql
@@ -23,6 +23,28 @@
 -- I was thinking about COUNT for this problem when I should have been thinking about SUM! Need
 -- to practice more I guess overall; but it's ok, I will get there! :)
 
+-------
+
+-- For the query:
+-- SELECT 
+--     *
+-- FROM Users u
+-- LEFT JOIN Orders o
+-- ON u.user_id=o.buyer_id;
+
+-- You get the table shown below:
+-- | user_id | join_date  | favorite_brand | order_id | order_date | item_id | buyer_id | seller_id |
+-- | ------- | ---------- | -------------- | -------- | ---------- | ------- | -------- | --------- |
+-- | 1       | 2018-01-01 | Lenovo         | 2        | 2018-08-02 | 2       | 1        | 3         |
+-- | 1       | 2018-01-01 | Lenovo         | 1        | 2019-08-01 | 4       | 1        | 2         |
+-- | 2       | 2018-02-09 | Samsung        | 6        | 2019-08-05 | 2       | 2        | 4         |
+-- | 2       | 2018-02-09 | Samsung        | 3        | 2019-08-03 | 3       | 2        | 3         |
+-- | 3       | 2018-01-19 | LG             | 5        | 2018-08-04 | 1       | 3        | 4         |
+-- | 4       | 2018-05-21 | HP             | 4        | 2018-08-04 | 1       | 4        | 2         |
+
+-- We want to keep the values within the year 2019, but we also don't want to remove the values that are not
+-- in 2019, i.e. user_id 3 and 4. Therefore, the WHERE clause is not appropriate for this use case right here.
+
 SELECT 
     u.user_id AS 'buyer_id',
     u.join_date,
