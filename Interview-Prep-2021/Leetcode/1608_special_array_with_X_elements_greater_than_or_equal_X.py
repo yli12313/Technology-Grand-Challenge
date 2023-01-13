3c2ad4dd2de1273327df1b44b12fad66d97ee071
Yingquan Li <orbit196@gmail.com>
2023-01-13 12:58:30 -0500

    Add files via upload

---
+++ Interview-Prep-2021/Leetcode/1608_special_array_with_X_elements_greater_than_or_equal_X.py
@@ -0,0 +1,27 @@
+# LINK: https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
+
+# NOTE: I don't know why I struggled with this problem so much. I tried many different wrong approaches.
+# I didn'treally understand the problem or what it was asking. I didn't realize that the problem wants you
+# to think in terms of 'i', which ranges between 0-1000. That's the answer space! From the answer space, you
+# want to find the number of elements in 'nums' greater than or equal to i, for example 'elem>=i' and keep a
+# count. Because the answer is unique, you just need to find the i that's equal to the count, for example
+# 'count==i'.
+
+# An FYI: the sorted() function always sorts in ascending order (i.e. the values increase and become larger).
+
+class Solution(object):
+  def specialArray(self, nums):
+    for i in range(0, 1001):
+      count=0
+
+      for elem in nums:
+        if elem>=i:
+          count+=1
+      
+      if count==i:
+        return i
+
+    return -1
+
+foo = Solution();
+print(foo.specialArray([3,9,7,8,3,8,6,6]));
