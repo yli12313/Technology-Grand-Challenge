af9b1d001eb169667a0cb16819d316bdaf03f7ce
Yingquan Li <orbit196@gmail.com>
2023-05-27 04:06:59 -0400

    Add files via upload

---
+++ Interview-Prep-2021/Leetcode/3_longest_substring_without_repeating_characters.py
@@ -0,0 +1,37 @@
+# LINK: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution(object):
+  def lengthOfLongestSubstring(self, s):
+    # Set the two pointers, the dictionary to keep track of the longest substring, and 
+    # the curren maximum substring length.
+    i = 0
+    j = 0
+    dic = {}
+    max_substring = 0
+
+    # While the second pointer is still in play, keep looping.
+    while j < len(s):
+      
+      # If a new character is found starting at index 0, add it to the dictionary.
+      # Increase the second pointer and calculate the length of the  current longest
+      # substring.
+      if s[j] not in dic.keys():
+        dic[s[j]] = 1
+        j += 1
+        distance = len(dic)
+        max_substring = max(distance, max_substring)
+
+      # If we found a duplicate character, pop() the exiting character that's 1/2 of
+      # the duplicate and increase the first pointer. The colde will go back to the while()
+      # loop and add the ducplicate value found in the next iteration of the while() loop. \
+      # The duplicate found is special because it could be part of the longest substring 
+      # for another substring that beyond the first substring. Increase the first pointer.
+      else:
+        dic.pop(s[i])
+        i += 1
+    
+    # Return the length of the maximum longest substring.
+    return max_substring
+
+foo = Solution();
+print(foo.lengthOfLongestSubstring("pwwkew"));
