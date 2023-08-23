18c6c54987c93852c57f6b6a9974dcf629d4e373
Yingquan Li <orbit196@gmail.com>
2023-08-23 16:52:04 -0400

    Update 134_gas_station.py

--- Interview-Prep-2021/Leetcode/134_gas_station.py
+++ Interview-Prep-2021/Leetcode/134_gas_station.py
@@ -8,14 +8,14 @@ class Solution(object):
     # SC: O(1)
 
     # NOTE: The key point is that: you can only form a cycle when the sum of all the values in [gas]
-    # is greater than or equal to the sum of all the values in [cost]. Given that in the for-loop() 
-    # the computation is 'total_gas += gas[i]-cost[i]', the computation that we have to therefore check
-    # in the final return statement is 'total_gas >= 0'.
+    # is greater than or equal to the sum of all the values in [cost]. Given that in the for() loop 
+    # the computation is 'total_gas += gas[i]-cost[i]', we therefore know that the computation in the 
+    # final return statement is 'total_gas >= 0'.
 
     # Setting the variables. When you set the problem up, you have to start with:
-    # 1) total_gas
-    # 2) current_gas
-    # 3) station
+    #   1) total_gas
+    #   2) current_gas
+    #   3) station
     total_gas = 0
     current_gas = 0
     station = 0
@@ -29,17 +29,16 @@ class Solution(object):
       current_gas += gas[i]-cost[i]
 
       # If you can't get to the next station from the current station, then reset
-      # the station and set current_gas = 0.
+      # the station 'station=i+1' and set 'current_gas = 0'.
       if current_gas < 0:
         station = i+1
         current_gas = 0
 
     # If the total_gas is non-negative once you reach the end, you've created a cycle
     # and you can return station. Else return -1. Moreover, the return statment here is of
-    
-    # the form: return exp if exp else exp
+    # the form: return (exp if exp else exp)
 
-    # The return statement should also be of the form: 'total_gas >= 0'
+    # The return statement's key trick is the following part: 'total_gas >= 0'
     return station if total_gas >= 0 else -1
 
 foo = Solution();
