8b34d574c2471671cb1b95450fbd6ffe6055e80f
Yingquan Li <orbit196@gmail.com>
2023-08-23 16:22:51 -0400

    Update 125_valid_palindrome.py

--- Interview-Prep-2021/Leetcode/125_valid_palindrome.py
+++ Interview-Prep-2021/Leetcode/125_valid_palindrome.py
@@ -1,7 +1,17 @@
-# Link: https://leetcode.com/problems/valid-palindrome/
+# LINK: https://leetcode.com/problems/valid-palindrome/
 
 class Solution(object):
+    
+    
     def isPalindrome(self, s):
+        # Constraints:
+        # - 1 <= s.length <= 2 * 10^5
+        # - s consists only of printable ASCII characters.
+
+        # Topic: Two pointers.
+
+        # Approach 1 (June, 2022):
+        """
         # If it's an empty string, return True that the string is a palindrome.
         if len(s) == "":
             return True
@@ -41,3 +51,77 @@ class Solution(object):
         
         # Return true by default.
         return True
+        """
+
+        # Approach 2:
+        # - Strip the string of non-alphanumeric characters and convert each character to 
+        # lower case.
+        # - Set pointer as the index of the first character and pointer as the 
+        # index of the last character.
+        # - Do a while loop where start < end. See if the characters match each other. If they
+        # don't return False. 
+        # - Increment the start pointer and decrement the end pointer.
+        # - Return True by default.
+
+        # TC: O(N)
+        # SC: O(N)
+
+        # NOTE: I had a solution from last June that works, but it's a very convoluted solution.
+        # It is a O(N) space complexity solution however. I'm learning a lot from the Neetcode 
+        # solutions however.
+
+        # The expression here is (where s is the collection!):
+        # (exp for c in s if exp)
+        """
+        s = "".join(c.lower() for c in s if c.isalnum())
+
+        start = 0
+        end = len(s)-1
+
+        while start < end:
+            if s[start] != s[end]:
+                return False
+            start += 1
+            end -= 1
+        
+        return True
+        """
+
+        # Approach 3: Trying the Neetcode solution. This solution is pretty interesting.
+        # - Define your own is alphanumeric function.
+        # - Define two pointers again.
+        # - Use two while() loops to to find only the characters we are looking for.
+        # - Compare the values from the two pointers and if they are not equal, return False.
+        # - Update pointers.
+        # - Return True.
+
+        # TC: O(N)
+        # SC: O(1)
+
+        l, r = 0, len(s)-1
+
+        while l<r:
+            # When you call a method in a class, you have to use 'self.'
+            while l<r and not self.isalnum(s[l]):
+                l += 1
+
+            while r>l and not self.isalnum(s[r]):
+                r -= 1
+
+            # Right here, you have to call the lower() function! The characters
+            # have not been converted to lowercase.
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        
+        return True
+
+    def isalnum(self, c):
+        """
+        This function only detected if a character is alphanumeric, but does no
+        conversion of the actual character!
+        """
+        return (ord('A') <= ord(c) <= ord('Z')) or \
+            (ord('a') <= ord(c) <= ord('z')) or \
+            (ord('0') <= ord(c) <= ord('9'))
