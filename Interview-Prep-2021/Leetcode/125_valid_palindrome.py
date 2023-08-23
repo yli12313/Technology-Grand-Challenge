d8f930a0d3423b5a7a1e07512069af190c177dea
Yingquan Li <orbit196@gmail.com>
2023-08-23 16:23:20 -0400

    Update 125_valid_palindrome.py

--- Interview-Prep-2021/Leetcode/125_valid_palindrome.py
+++ Interview-Prep-2021/Leetcode/125_valid_palindrome.py
@@ -1,9 +1,8 @@
 # LINK: https://leetcode.com/problems/valid-palindrome/
 
 class Solution(object):
-    
-    
     def isPalindrome(self, s):
+        
         # Constraints:
         # - 1 <= s.length <= 2 * 10^5
         # - s consists only of printable ASCII characters.
