e303672492ffc10f3f63989b8428fd6c1d89ecc7
Yingquan Li <orbit196@gmail.com>
2023-08-23 16:54:09 -0400

    Update 274_H_index.py

--- Interview-Prep-2021/Leetcode/274_H_index.py
+++ Interview-Prep-2021/Leetcode/274_H_index.py
@@ -5,20 +5,18 @@ class Solution(object):
     # Topic: dynamic programming (my guess is wrong). It's a 
     # sorting/counting sort problem.
 
+    # Approach 1:
+    # - Sort the citations in reverse order with the paper that the highest citation
+    # at the start.
+    # - Set the H-index counter to 0. 
+    # - Do an enumeration with the index,citation pairs in citations starting at index=1.
+    # - We know that as long as index <= citation, we have 'index' number of papers with 
+    # at least 'citation'. Update and increase the H-index.
+    # - Else you can break and you know that you found the max H-index.
+    # - Return the H-index.
+
     # TC: O(Nlog(N))
     # SC: O(1)
-
-    ####
-
-    # Approach 1:
-    # 1) Sort the citations in reverse order with the paper with the highest citation
-    # at the start.
-    # 2) Set the H-index counter to 0. 
-    # 3) Do an enumeration with the index,citation pairs in citations starting at index=1.
-    # 4) We know that as long as index <= citation, we have 'index' number of papers with 
-    # at least 'citation'. Update and increase the H-index.
-    # 5) Else you can break and you know that you found the max H-index.
-    # 6) Return the H-index.
 
     citations.sort(reverse=True)
 
