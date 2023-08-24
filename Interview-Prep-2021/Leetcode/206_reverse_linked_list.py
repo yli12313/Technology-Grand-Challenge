0c15d2eed72af30a96a197521161e229100aa461
Yingquan Li <orbit196@gmail.com>
2023-08-24 00:02:00 -0400

    Create 206_reverse_linked_list.py

---
+++ Interview-Prep-2021/Leetcode/206_reverse_linked_list.py
@@ -0,0 +1,72 @@
+# LINK: https://leetcode.com/problems/reverse-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def reverseList(self, head):
+        # Constraints
+        # - The number of nodes in the list is in the range [0, 5000]
+        # - -5000 <= Node.val <= 5000
+
+        # Topic: Linked List and Recursion.
+
+        # Approach 1 (iterative approach):
+        # - The iterative approach is not hard. You have to set a temp
+        # variable to 'cur.next'; the you can begin updating pointers such 
+        # that you update: 
+
+        #    1) cur.next
+        #    2) prev
+        #    3) cur
+
+        # TC: O(N)
+        # SC: O(1)
+
+        """
+        cur = head
+        prev = None
+
+        while cur != None:
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+        
+        return prev
+        """
+
+        # Approach 2 (Recursive Approach):
+        # - Not exactly sure what's going on, but this is how you do it recursively.
+        # - UPDATE: I was able to understand it! Don't quit and keep trying!
+
+        # NOTE: The best way to think about a recursive problem is to break it down into 
+        # a sub-problem.
+
+        # TC: O(N)
+        # SC: O(N)
+
+        # Base case: If the head or head.next is None, then return the head. Can't reverse
+        # the linked list in these two cases!
+        if head == None or head.next == None:
+            return head
+        
+        # Let the newHead recurse to the very end of the linked list. This is the pointer
+        # that we will return in the end as the answer!
+        newHead = self.reverseList(head.next)
+
+        # After newHead points to the last node in the linked list, the recursion jumps
+        # back and now you are at the SECOND TO LAST NODE. You set:
+
+        # 1) head.next.next -> head
+        # 2) head.next -> None
+
+        # This is just genius!
+
+        head.next.next = head
+        head.next = None
+
+        return newHead
