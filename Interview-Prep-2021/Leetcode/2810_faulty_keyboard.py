f219e5f014bf99c8fe3d8e6e9c68a0419b8409ae
Yingquan Li <orbit196@gmail.com>
2023-08-30 12:56:17 -0400

    Create 2810_faulty_keyboard.py

---
+++ Interview-Prep-2021/Leetcode/2810_faulty_keyboard.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def finalString(self, s):
+        # Constraints
+        # - 1 <= s.length <= 100
+        # s consists of lowercase English letters.
+        # s[0] != 'i'
+
+        # Topic: Array.
+        # This is a String, Simulation problem.
+
+        # Approach 1:
+        # - Loop through the array.
+        # - If you encounter i where the index of i is greater than 1, then you reverse the new string.
+        # - Else add the character to the new string. 
+        # - Return the new string.
+
+        # TC: O(N)
+        # SC: O(N)
+
+        answer = ""
+
+        for j in range(len(s)):
+            c = s[j]
+
+            if c != 'i':
+                answer += c
+            elif c == 'i':
+                answer = answer[::-1]
+        
+        return answer
