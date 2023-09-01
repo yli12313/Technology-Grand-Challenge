612099cbc977ba295558f788e7c0a6e2bc824e79
Yingquan Li <orbit196@gmail.com>
2023-09-01 14:18:53 -0400

    Update 2815_max_pair_sum_in_an_array.py

--- Interview-Prep-2021/Leetcode/2815_max_pair_sum_in_an_array.py
+++ Interview-Prep-2021/Leetcode/2815_max_pair_sum_in_an_array.py
@@ -1,33 +1,36 @@
 class Solution(object):
+    def findMax(self, n):
+        # TRICK: Define 'ans' right here.
+        ans = -1
+        
+        while n > 0:
+            # REMINDER: 'n%10' will get you the digit that is right-most.
+            # REMINDER: 'n/10' will cut the digit that is right-most.
+            r=n%10
+            ans = max(ans, r)
+            # TRICK: you have to do 'math.floor(n/10)'.
+            n=math.floor(n/10)
+        
+        return ans
+    
     def maxSum(self, nums):
-        # Constraints
+        # Constraints:
         # - 2 <= nums.length <= 100
         # - 1 <= nums[i] <= 10^4
 
         # Topic: Array.
-        # This is an array and hash table problem.
+        # This is an Array and Hash Table problem.
 
         # Approach 1:
-        # - Wouldn't you just do:
-        # [51,71,17,24,42] -> [6,8,8,6,6]
-        # - Then you would need to extract the maximum number that has 
-        # frequency of 2?
-        # {8:2, 6:3}
-        # - This is just not the right approach; it's ok to look at the
-        # answers in this case.
-
-        # (HINT): Find the largest and second largest element with maximum
-        # digits equal to x where 1<=x<=9.
-        
-        # - Ah! You have to find the MAXIMUM SUM of a PAIR OF NUMBERS where
-        # the MAXIMUM DIGIT in both numbers are EQUAL. Else return -1.
-        # - (Full Algorithm): Define a function that will calculate the max
-        # digit in any number.
-        # - Define 'ans'
-        # - Define double for() loops that will loop through the array 'nums'.
-        # - Call max digit function on numbers: 'nums[i]' and 'nums[j]'.
-        # - If the two numbers have the same max digit, calcuate the sum and
-        # update 'ans'.
+        # - Define a findMax() method in the class 'Solution' that will find 
+        # the maximum digit in any number.
+        # - Define an answer variable.
+        # - Do double for() loops that will iterate through the numbers n in nums.
+        # - Call our findMax() method on nums[i] and nums[j].
+        # - If the two numbers have equal max digits, then calculate the sum of
+        # the two numbers.
+        # - Update the answer as the max of the previous answer and the new sum.
+        # - Return the answer.
 
         # TC: O(N^2)
         # SC: O(1)
@@ -47,18 +50,3 @@ class Solution(object):
                     ans=max(ans, sum)
         
         return -1 if ans == -1 else ans
-
-    def findMax(self, n):
-        # TRICK: Define 'ans' right here.
-        ans = -1
-        
-        while n > 0:
-            # REMINDER: 'n%10' will get you the digit that is right-most.
-            # REMINDER: 'n/10' will cut the digit that is right-most.
-
-            r=n%10
-            ans = max(ans, r)
-            # TRICK: you have to do 'math.floor(n/10)'.
-            n=math.floor(n/10)
-        
-        return ans
