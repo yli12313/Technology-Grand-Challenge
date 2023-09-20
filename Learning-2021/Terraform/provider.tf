71d0567200eec352c1ed5fffd113829c72cfe001
Yingquan Li <orbit196@gmail.com>
2023-09-20 18:44:47 -0400

    Update provider.tf

--- Learning-2021/Terraform/provider.tf
+++ Learning-2021/Terraform/provider.tf
@@ -2,8 +2,8 @@ provider "aws" {
 	region     = "us-east-1"
 
 	# Access key for CLI access
-	access_key = "AKIAXBWLZIO2CYLP2SZ2"  
+	access_key = ""  
 
 	# Secret access key for CLI access
-	secret_key = "9+CgZvUvs3WGACANC3N1HvweKiaXgiEUhoH0bvPp"
+	secret_key = ""
 }
