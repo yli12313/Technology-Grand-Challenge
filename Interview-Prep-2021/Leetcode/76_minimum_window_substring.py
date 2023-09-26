437524ab85e501839aba1ab57007b10a06d415f4
Yingquan Li <orbit196@gmail.com>
2023-09-26 15:05:03 -0400

    Update 76_minimum_window_substring.py

--- Interview-Prep-2021/Leetcode/76_minimum_window_substring.py
+++ Interview-Prep-2021/Leetcode/76_minimum_window_substring.py
@@ -42,5 +42,35 @@ class Solution(object):
         # - Do 'l,r = res'.
         # - Return 's[l:r+1]' if 'resLen != float("infinity")' else ''.
 
+        if t == "":
+            return ""
 
+        countT,window = {},{}
+
+        for c in t:
+            countT[c] = 1+countT.get(c,0)
         
+        have,need = 0,len(countT)
+        res,resLen = [-1,-1],float("infinity")
+        l = 0
+
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1+window.get(c,0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+            
+            while have == need:
+                if (r-l+1) < resLen:
+                    res = [l,r]
+                    resLen = r-l+1
+                # TRICK: Make sure you get this part right! 'window[c] -= 1', where 'c = s[l]'.
+                c = s[l]
+                window[c] -= 1
+                if c in countT and window[c] < countT[c]:
+                    have -= 1
+                l += 1
+            
+        l,r = res
+        return s[l:r+1] if resLen != float("infinity") else ""
