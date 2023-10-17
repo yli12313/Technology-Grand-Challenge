2b3f31966310ec7fc38162d9b597a36076d80631
Yingquan Li <orbit196@gmail.com>
2023-10-16 22:16:33 -0400

    Update 2873_maximum_value_of_an_ordered_triplet_I.py

--- Interview-Prep-2021/Leetcode/2873_maximum_value_of_an_ordered_triplet_I.py
+++ Interview-Prep-2021/Leetcode/2873_maximum_value_of_an_ordered_triplet_I.py
@@ -16,12 +16,19 @@ class Solution(object):
         # - Calculate the result.
         # - Return the result.
 
+        # Approach 1 (YouTube):
+        # - This is a triple for loop problem (Setup the skeleton first).
+        # - The first for loop goes from 0..n-2.
+        # - The second for loop goes from i+1..len(n-1).
+        # - The thrid for loop goes from i+2..n.
+
         # TC: O(N^3)
         # SC: O(1)
         
         n = len(nums)
         res = 0
 
+        # TRICK: Make sure to get the upper boundaries correct: n-2,n-1,n.
         for i in range(n-2):
             for j in range(i+1,n-1):
                 for k in range(j+1,n):
