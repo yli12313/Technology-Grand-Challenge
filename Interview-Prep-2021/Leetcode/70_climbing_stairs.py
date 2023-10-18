d24b7779090a2c23b0d828771f35b9dc5bfcaa81
Yingquan Li <orbit196@gmail.com>
2023-10-17 23:13:01 -0400

    Update 70_climbing_stairs.py

--- Interview-Prep-2021/Leetcode/70_climbing_stairs.py
+++ Interview-Prep-2021/Leetcode/70_climbing_stairs.py
@@ -6,13 +6,38 @@ class Solution(object):
         # - 1 <= n <= 45
 
         # Topic: Dynamic Programming
-        # This is a Math, Dynamic Programming or Memoization problem.
+        # This could also be a Math, Dynamic Programming, or Memoization problem.
 
-        # Approach 1 (iterative):
+        # Approach 1 (dynamic programming):
+        # - Define a results array that will host the answer.
+        # - Treat the n == 1 case ans n >= 2 case.
+        # - Loop from 2..n.
+        # - Update the results array.
+        # - Return the right most element in the results array.
+
+        # TC: O(N)
+        # SC: O(N)
+
+        """
+        res = [0]*n
+        if n == 1:
+            res[0] = 1
+        elif n >= 2:
+            res[0] = 1
+            res[1] = 2
+        
+        for i in range(2,n):
+            res[i] = res[i-1]+res[i-2]
+        
+        return res[n-1]
+        """
+
+        # Approach 2: (iterative):
         # - Treat the n == 1 case.
-        # - Define first and second as 1 and 2.
+        # - Define first and second and set them to 1,2.
         # - Loop from 3..n+1.
-        # - Update third, first and second. 
+        # - Create a variable third that's first+second.
+        # - Update first; update second.
         # - Return second.
 
         # TC: O(N)
@@ -23,38 +48,10 @@ class Solution(object):
         
         first = 1
         second = 2
-        
+
         for i in range(3,n+1):
             third = first+second
             first = second
             second = third
         
         return second
-
-        # Approach 1 (dynamic programming):
-        # - Define an array that will store previously computed values
-        # of size n. 
-        # - If n == 1, update the first value in the results array.
-        # - Else if n == 2, update the first and the second value in the
-        # results array.
-        # - Loop from 2..n.
-        # - Update the results array.
-        # - Return the right most value in the results array.
-
-        # TC: O(N)
-        # SC: O(N)
-
-        res = [0]*n
-        if n == 1:
-            res[0] = 1
-        elif n >= 2:
-            res[0] = 1
-            res[1] = 2
-        
-        for i in range(2,n):
-            res[i] = res[i-1]+res[i-2]
-
-        return res[n-1]
-
-foo = Solution();
-print(foo.climbStairs(3))
