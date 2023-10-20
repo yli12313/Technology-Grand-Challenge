73d0ad4f2ac726cfb74bc6443881dcecbd03541a
Yingquan Li <orbit196@gmail.com>
2023-10-19 20:17:26 -0400

    Update SS_salaries_differences.sql

--- Interview-Prep-2021/SQL/Other/SS_salaries_differences.sql
+++ Interview-Prep-2021/SQL/Other/SS_salaries_differences.sql
@@ -1,4 +1,7 @@
 -- LINK: https://platform.stratascratch.com/coding/10308-salaries-differences?code_type=3
+-- TRICKS:
+-- Make sure you have the correct id for the department table 'department_id'.
+-- Make sure you have insightful names for each table!
 
 SELECT ABS(
 (SELECT max(salary) 
