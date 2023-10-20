f8123c7b4edbc0efd62148153a5e5a73f452fd81
Yingquan Li <orbit196@gmail.com>
2023-10-19 20:21:22 -0400

    Update SS_salaries_differences.sql

--- Interview-Prep-2021/SQL/Other/SS_salaries_differences.sql
+++ Interview-Prep-2021/SQL/Other/SS_salaries_differences.sql
@@ -1,15 +1,16 @@
 -- LINK: https://platform.stratascratch.com/coding/10308-salaries-differences?code_type=3
 -- TRICKS:
--- Make sure you have the correct id for the department table 'department_id'.
--- Make sure you have insightful names for each table!
+-- 1) Make sure you have the correct id for the department table 'department_id'.
+-- 2) Make sure you have insightful names for each table!
+-- 3) Make sure that you actually select 'MAX(salary)' from each of the subqueries!
 
 SELECT ABS(
-(SELECT max(salary) 
+(SELECT MAX(salary) 
 FROM db_employee emp 
 JOIN db_dept dept ON emp.department_id = dept.id 
 WHERE department = "marketing")
 -
-(SELECT max(salary) 
+(SELECT MAX(salary) 
 FROM db_employee emp 
 JOIN db_dept dept ON emp.department_id = dept.id 
 WHERE department = "engineering")) AS salary_diff;
