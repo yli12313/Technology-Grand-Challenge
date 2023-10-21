a6800b63d65a1d87fd752c5688c49db561d38c0d
Yingquan Li <orbit196@gmail.com>
2023-10-21 15:58:55 -0400

    Create SS_average_salaries.sql

---
+++ Interview-Prep-2021/SQL/Other/SS_average_salaries.sql
@@ -0,0 +1,12 @@
+-- LINK: https://platform.stratascratch.com/coding/9917-average-salaries?code_type=3
+
+-- TRICK: 
+-- No notes to record with this one; good problem!
+-- Make sure to have good variable names and be confidence with the variable names that you choose!
+
+SELECT
+    department,
+    first_name,
+    salary,
+    AVG(salary) OVER (PARTITION BY department) AS dept_avg_salary
+FROM employee e
