4a12e019bad50ae8b43f6386202f868c79323ed7
Yingquan Li <orbit196@gmail.com>
2023-10-21 15:51:16 -0400

    Create SS_customer_details.sql

---
+++ Interview-Prep-2021/SQL/Other/SS_customer_details.sql
@@ -0,0 +1,26 @@
+-- LINK: https://platform.stratascratch.com/coding/9891-customer-details?code_type=3
+
+-- TRICKS:
+-- Have to just keep practicing the syntax for joins! It's <JOIN> [TABLE] <ON> [CONDITION].
+
+-- Version 1:
+/*
+SELECT
+    first_name,
+    last_name,
+    city,
+    order_details
+FROM customers c
+LEFT JOIN orders o ON c.id = o.cust_id
+ORDER BY first_name,order_details;
+*/
+
+-- Version 2:
+SELECT
+    first_name,
+    last_name,
+    city,
+    order_details
+FROM customers c
+LEFT JOIN orders o ON c.id = o.cust_id
+ORDER BY 1,4;
