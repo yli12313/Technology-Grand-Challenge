246ce9727bdf04a5075e0f83362529afa9ea70fd
Yingquan Li <orbit196@gmail.com>
2023-10-23 01:32:29 -0400

    Add files via upload

---
+++ Interview-Prep-2021/Leetcode/Other/Striver/STRIVER_pattern_problems.py
@@ -0,0 +1,31 @@
+# 1
+"""
+for i in range(1,6):
+    for j in range(1,6):
+        print("*",end="")
+    print()
+"""
+
+# 2
+"""
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+"""
+
+# 3
+"""
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
+"""
+
+# 4
+"""
+for i in range(1,6):
+    for j in range(0,i):
+        print(i,end="")
+    print()
+"""
