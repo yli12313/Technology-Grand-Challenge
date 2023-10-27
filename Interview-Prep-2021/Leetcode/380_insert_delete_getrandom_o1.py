63c7722c5d9aee00076632ef2df727078c76fc40
Yingquan Li <orbit196@gmail.com>
2023-10-27 04:52:54 -0400

    Update 380_insert_delete_getrandom_o1.py

--- Interview-Prep-2021/Leetcode/380_insert_delete_getrandom_o1.py
+++ Interview-Prep-2021/Leetcode/380_insert_delete_getrandom_o1.py
@@ -1,45 +1,64 @@
 # LINK: https://leetcode.com/problems/insert-delete-getrandom-o1/
 
-# Topic: Array
-# This is an Array, Hash Table, Math, Design, or Randomized problem.
-
-# TC: O(1)
-# SC: O(N)
-
 class RandomizedSet(object):
+    # Constraints
+    # - -2^31 <= val <= 2^31 - 1.
+    # - At most 2 * 10^5 calls will be made to insert, remove, and getRandom.
+    # - There will be at least one element in the data structure when getRandom is called.
+
+    # Topic: Array
+    # This is an Array, Hash Table, Math, Design or Randomized problem.
+
+    # TC: O(1)
+    # SC: O(N)
+
     def __init__(self):
+        # Ex:
+        # Insert(1)
+        # Insert(2)
+        # Insert(3)
+
+        # self.ind = {1:0, 2:1, 3:2}
+        # self.values = [1,2,3]
+
         self.ind = {}
-        # TRICK: The 'self.values' has to be a list.
         self.values = []
         
     def insert(self, val):
+        # Approach 1:
+        # - If val is in self.ind, then return False.
+        # - Append val to values.
+        # - Update self.ind for val such that it's index is the value, where val is the key!
+
         if val in self.ind:
             return False
         
         self.values.append(val)
         self.ind[val] = len(self.values)-1
         return True
-
+        
     def remove(self, val):
+        # Approach 1:
+        # - If val is not in self.ind, then return False.
+        # - Get the index i for val.
+        # - Set the index of the last entry in val to have index i.
+        # - Set the value of the entry in the list at index i to be the last entry.
+        # - Delete the entry for val in the indices dictionary; delete the last value from the list.
+
         if val not in self.ind:
             return False
-
-        # TRICK: With remove(), we have a four step process:
-        # 1) Get the index of val (i).
-        # 2) Set the index of the last value in the array to i.
-        # 3) Set the ith value in the array to the last value.
-        # 4) Pop val from the indices; pop the last value from the array.
         
         i = self.ind[val]
         self.ind[self.values[-1]] = i
         self.values[i] = self.values[-1]
-        
+
         self.ind.pop(val)
         self.values.pop()
         return True
-
+        
     def getRandom(self):
-        # TRICK: The trick here is 'random.choice(self.values)'.
+        # Approach 1: Use the function 'random.choice(...)'.
+
         return random.choice(self.values)
         
 # Your RandomizedSet object will be instantiated and called as such:
