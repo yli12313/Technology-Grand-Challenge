b08173b5f640610af1620b175dc723968f5d0ff0
Yingquan Li <orbit196@gmail.com>
2023-11-04 13:51:58 -0400

    Create 102_binary_tree_level_order_traversal.py

---
+++ Interview-Prep-2021/Leetcode/102_binary_tree_level_order_traversal.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    # TC: O(N)
+    # SC: O(N/2) -> O(N)
+
+    def levelOrder(self, root):
+        res = []
+
+        if not root:
+            return res
+
+        queue = []
+        queue.append(root)
+
+        while queue:
+            level = []
+
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                if node:
+                    level.append(node.val)
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:  
+                        queue.append(node.right)
+            
+            res.append(level)
+    
+        return res
