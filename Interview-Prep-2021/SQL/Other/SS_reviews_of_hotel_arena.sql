9d4f700db006e55a587038a1f0dcef09aed58f95
Yingquan Li <orbit196@gmail.com>
2023-11-15 15:35:48 -0500

    Create SS_reviews_of_hotel_arena.sql

---
+++ Interview-Prep-2021/SQL/Other/SS_reviews_of_hotel_arena.sql
@@ -0,0 +1,12 @@
+-- LINK: https://platform.stratascratch.com/coding/10166-reviews-of-hotel-arena?code_type=3
+
+-- TRICKS:
+-- SQL is not hard at all; you just need to consistently practice everyday!
+
+SELECT 
+    hotel_name,
+    reviewer_score,
+    COUNT(reviewer_score) AS score_count
+FROM hotel_reviews
+WHERE hotel_name = "Hotel Arena"
+GROUP BY reviewer_score
