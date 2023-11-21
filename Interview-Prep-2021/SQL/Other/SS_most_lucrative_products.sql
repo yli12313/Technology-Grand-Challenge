2055b2b9fc9e9955a69649453580fadd8cf1c4d9
Yingquan Li <orbit196@gmail.com>
2023-11-21 13:18:43 -0500

    Create SS_most_lucrative_products.sql

---
+++ Interview-Prep-2021/SQL/Other/SS_most_lucrative_products.sql
@@ -0,0 +1,16 @@
+-- LINK: https://platform.stratascratch.com/coding/2119-most-lucrative-products?code_type=3
+
+-- TRICKS:
+-- When you want to find something that's between two intervals, you can use the 'where' clause with
+-- 'between..and'.
+-- Example: 'where month(date) between 1 and 6'.
+
+select 
+    product_id,
+    sum(cost_in_dollars*units_sold) as total_revenue
+from 
+online_orders
+where month(date) between 1 and 6
+group by product_id
+order by total_revenue desc
+limit 5;
