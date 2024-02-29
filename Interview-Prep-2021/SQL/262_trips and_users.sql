ab1345e0c7e3bd4789e21d974b67c5b96917eddb
Yingquan Li <orbit196@gmail.com>
2024-02-29 16:16:01 -0500

    Create 262_trips and_users.sql

---
+++ Interview-Prep-2021/SQL/262_trips and_users.sql
@@ -0,0 +1,15 @@
+# LINK: https://leetcode.com/problems/trips-and-users/
+
+select
+    request_at as Day,
+    round(
+        count(if(status in ('cancelled_by_driver', 'cancelled_by_client'), 1, null))
+        / count(*)
+    , 2) as 'Cancellation Rate'
+from Trips
+where client_id
+    in (select users_id from Users where banned = 'No')
+    and driver_id
+    in (select users_id from Users where banned = 'No')
+GROUP BY request_at
+having request_at between '2013-10-01' and '2013-10-03'
