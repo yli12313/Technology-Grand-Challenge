ae79d14342267a2f2a86ec4e2ac4e3c47aa55c7e
Yingquan Li <orbit196@gmail.com>
2024-03-06 17:08:01 -0500

    Update CN_1_to_n_without_loop.py

--- Interview-Prep-2021/Leetcode/Other/Striver/CN_1_to_n_without_loop.py
+++ Interview-Prep-2021/Leetcode/Other/Striver/CN_1_to_n_without_loop.py
@@ -1,7 +1,7 @@
 # LINK: https://www.codingninjas.com/studio/problems/print-1-to-n_628290?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
 
 # TRICKS:
-# - You should do the 1) recursive step first in the auxiliary function before 2) adding the value to the container.
+# - You should do the 1) recursive step first in the auxiliary function before 2) appending the value to the container.
 # - This saves you an 'answer.reverse()' operation!
 
 # TC: O(N)
