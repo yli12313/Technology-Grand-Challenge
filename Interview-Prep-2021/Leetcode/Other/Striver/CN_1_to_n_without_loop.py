f9d363f0b29b97f5b8229ee245dc4661ed3489c4
Yingquan Li <orbit196@gmail.com>
2024-03-06 17:02:15 -0500

    Update CN_1_to_n_without_loop.py

--- Interview-Prep-2021/Leetcode/Other/Striver/CN_1_to_n_without_loop.py
+++ Interview-Prep-2021/Leetcode/Other/Striver/CN_1_to_n_without_loop.py
@@ -1,19 +1,24 @@
 # LINK: https://www.codingninjas.com/studio/problems/print-1-to-n_628290?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
 
+# TRICKS:
+# - You should do the 1) recursive step first in the auxiliary function before 2) adding the value to the container.
+# - This saves you an 'answer.reverse()' operation!
+
+# TC: O(N)
+# SC: O(N)
+
 from typing import List
 
 def recursive(container, n):
     if n <= 0:
         return
     
-    container.append(n)
-
     recursive(container,n-1)
+    container.append(n)
 
 def printNos(x: int) -> List[int]: 
     answer = []
 
     recursive(answer, x)
-    answer.reverse()
 
     return answer
