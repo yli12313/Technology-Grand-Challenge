7ac0d9a354d5c5644b9c35bc77b7d5b6a7259b92
Yingquan Li <orbit196@gmail.com>
2024-03-12 13:53:44 -0400

    Create CN_ largest_element_in_the_array.py

---
+++ Interview-Prep-2021/Leetcode/Other/Striver/CN_ largest_element_in_the_array.py
@@ -0,0 +1,17 @@
+# LINK: https://www.codingninjas.com/studio/problems/largest-element-in-the-array-largest-element-in-the-array_5026279?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+# TC: O(N)
+# SC: O(1)
+
+from sys import *
+from collections import *
+from math import *
+
+def largestElement(arr: [], n: int) -> int:
+    largest = 0
+
+    for x in arr:
+        if x > largest:
+            largest = x
+    
+    return largest
