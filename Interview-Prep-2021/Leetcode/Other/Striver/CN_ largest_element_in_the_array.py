088ad3716bc92a119046ace2a0a9f10a98585457
Yingquan Li <orbit196@gmail.com>
2024-03-12 13:54:08 -0400

    Update CN_ largest_element_in_the_array.py

--- Interview-Prep-2021/Leetcode/Other/Striver/CN_ largest_element_in_the_array.py
+++ Interview-Prep-2021/Leetcode/Other/Striver/CN_ largest_element_in_the_array.py
@@ -1,5 +1,8 @@
 # LINK: https://www.codingninjas.com/studio/problems/largest-element-in-the-array-largest-element-in-the-array_5026279?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
 
+# TRICK:
+# Keep going and keep practicing!
+
 # TC: O(N)
 # SC: O(1)
 
