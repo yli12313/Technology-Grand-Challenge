b611fe80335f4fdd632c45506819c967a89a74f0
Yingquan Li <orbit196@gmail.com>
2024-03-12 21:00:25 -0400

    Create CN_second_largest_number.py

---
+++ Interview-Prep-2021/Leetcode/Other/Striver/CN_second_largest_number.py
@@ -0,0 +1,56 @@
+# LINK: https://www.codingninjas.com/studio/problems/ninja-and-the-second-order-elements_6581960?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION
+
+def getSecondOrderElements(n: int,  a: [int]) -> [int]:
+    # array 'a' of 'n' unique non-negative integers
+    # find second largest and second smallest element from the array
+
+    # Approach 1 (Non-Optimal Solution):
+
+    # TC: O(N*Log(N)), don't forget that sorting is O(N*Log(N))!
+    # SC: O(1)
+
+    # if n <= 1:
+    #     return None
+    
+    # a.sort()
+    # return [a[-2], a[1]]
+
+    # Approach 2 (Optimized Solution):
+    # - This is the right code, but it only passes 10/11 tests. I think there's something wrong 
+    # with the Coding Ninjas platform.
+
+    # TC: O(N)
+    # SC: O(1)
+
+    if n < 2:
+        return None
+    
+    largest = sec_largest = smallest = sec_smallest = 0
+    
+    if a[0] > a[1]:
+        largest = sec_smallest = a[0]
+        smallest = sec_largest = a[1]
+        
+    else:
+        smallest = sec_largest = a[0]
+        largest = sec_smallest = a[1]
+    
+    for i in range(2,n):
+        cur = a[i]
+
+        if cur > largest:
+            sec_largest = largest
+            largest = cur
+        elif cur > sec_largest:
+            sec_largest = cur
+    
+    for i in range(2,n):
+        cur = a[i]
+
+        if cur < smallest:
+            sec_smallest = smallest
+            smallest = cur
+        elif cur < sec_smallest:
+            sec_smallest = cur
+    
+    return [sec_largest, sec_smallest]
